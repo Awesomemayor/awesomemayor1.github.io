e9d50fc3b4ac3e964d83d2d1474c829dec5d8b4c
Awesomemayor <adejumoolumayor@gmail.com>
2022-06-24 08:30:33 +0100

    displayed my name with javascript prompt

---
+++ script.js
@@ -0,0 +1,14 @@
+let firstName = "Mayowa" ;
+let middleName= "Moses";
+let lastName  = "Adejumo" ;
+let height= "6.4";
+let country ="Nigeria";
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+console.log(height);
+console.log(country);
+
+
+alert("Hi, you are welcome. I am Mayowa Adejumo. A Nigerian standing at 6'4.")
